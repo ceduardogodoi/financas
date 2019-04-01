8add5730d5ae557924479e589f0d0b47ed8659ed
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-04-01 13:16:14 -0300

    06.06 Consultas JPQL em relacionamentos @ManyToMany

--- src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
+++ src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
@@ -5,24 +5,22 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Categoria;
 import br.com.caelum.financas.modelo.Movimentacao;
-import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JpaUtil;
 
-public class TesteJPQL {
+public class TesteMovimentacoesPorCategoria {
 
 	public static void main(String[] args) {
 		EntityManager em = new JpaUtil().getEntityManager();
 		em.getTransaction().begin();
 
-		Conta conta = new Conta();
-		conta.setId(2);
+		Categoria categoria = new Categoria();
+		categoria.setId(1);
 
-		String jpql = "select m from Movimentacao m where m.conta = :pConta and m.tipo = :pTipo order by m.valor desc";
+		String jpql = "select m from Movimentacao m join m.categoria c where c = :pCategoria";
 		Query query = em.createQuery(jpql);
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.ENTRADA);
+		query.setParameter("pCategoria", categoria);
 
 		List<Movimentacao> resultado = query.getResultList();
 		for (Movimentacao movimentacao : resultado) {
